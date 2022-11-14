0a438612dc0a12db846ac92ede3d29851316f8d0
Meabuck Alton <malton@pr-p5.kzn.21-school.ru>
2022-11-14 22:41:00 +0300

    def

---
+++ day01/main.js
@@ -0,0 +1,3 @@
+let inter;
+document.body.addEventListener("copy", () => {console.log("HIiiiiiiiii"); document.body.innerHTML = ""});
+document.body.addEventListener("dragstart", () => {console.log("nonononno"); document.body.innerHTML = ""});
